5daf27b0568c491736d40bd069f796f93540f674
lpalazzi <lpalazzi@outlook.com>
2023-05-16 17:35:52 -0400

    ignore errors in update_segments script

--- scripts/update_segments.sh
+++ scripts/update_segments.sh
@@ -8,13 +8,13 @@ update_segments () {
     do
       echo ""
       echo "Downloading E${E}_N${N}"
-      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/E${E}_N${N}.rd5 http://brouter.de/brouter/segments4/E${E}_N${N}.rd5
+      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/E${E}_N${N}.rd5 http://brouter.de/brouter/segments4/E${E}_N${N}.rd5 || true
     done
     for S in $(seq 0 5 90)
     do
       echo ""
       echo "Downloading E${E}_S${S}"
-      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/E${E}_S${S}.rd5 http://brouter.de/brouter/segments4/E${E}_S${S}.rd5
+      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/E${E}_S${S}.rd5 http://brouter.de/brouter/segments4/E${E}_S${S}.rd5 || true
     done
   done
   for W in $(seq 0 5 180)
@@ -23,13 +23,13 @@ update_segments () {
     do
       echo ""
       echo "Downloading W${W}_N${N}"
-      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/W${W}_N${N}.rd5 http://brouter.de/brouter/segments4/W${W}_N${N}.rd5
+      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/W${W}_N${N}.rd5 http://brouter.de/brouter/segments4/W${W}_N${N}.rd5 || true
     done
     for S in $(seq 0 5 90)
     do
       echo ""
       echo "Downloading W${W}_S${S}"
-      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/W${W}_S${S}.rd5 http://brouter.de/brouter/segments4/W${W}_S${S}.rd5
+      curl -f -o "$(dirname "${BASH_SOURCE[0]}")"/../misc/segments4/W${W}_S${S}.rd5 http://brouter.de/brouter/segments4/W${W}_S${S}.rd5 || true
     done
   done
 }
